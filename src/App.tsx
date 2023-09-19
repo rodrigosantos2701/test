ded2c112823d3fd05ad41f0230bbdabc70f48eb3
Rodrigo.Santos <rodrigo.santos-ext@dalenys.com>
2023-09-19 12:06:48 -0300

    rebase 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   const test4 = 444444;
   const test4444 = 4444444;
   const test7 = 77777;
+  const test6 = 66666;
 
   //esta é a main
 
@@ -45,6 +46,7 @@ function App() {
       <h1>{test4}</h1>
       <h1>{test4444}</h1>
       <h1>{test7}</h1>
+      <h1>{test6}</h1>
     </>
   );
 }
